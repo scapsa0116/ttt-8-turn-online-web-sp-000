27ed09faf5e46ca07f531674bddddc7b133a8818
Stela Capsa <stela.capsa@gmail.com>
2019-10-28 16:08:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,9 @@
-def display_board(board)
- def valid_move?(board, index)
-  def position_taken?(board, index)
-   def input_to_index(user_input)
-    def move(board, index, first_player = "X")
-    end
+# def display_board(board)
+#  def valid_move?(board, index)
+#   def position_taken?(board, index)
+#    def input_to_index(user_input)
+#     def move(board, index, first_player = "X")
+#     end
 def turn(board)
   puts "Please enter 1-9:"
   num = gets.chomp
