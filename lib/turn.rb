9bc0223f5c333d8fc5aef5e3303b3686931d0ca2
Stela Capsa <stela.capsa@gmail.com>
2019-10-28 16:38:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,7 +14,6 @@ def display_board(board)
   puts board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
   board
 
-
    puts " #{board[0]} | #{board[1]} | #{board[2]} "
    puts "-----------"
    puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -31,6 +30,9 @@ def position_taken?(board, index)
    else
       return true
    end
+end
+def input_to_index(input)
+  input.to_i - 1
 end
 def valid_move?(board, index)
    if index.between?(0,8) && !position_taken?(board, index)
