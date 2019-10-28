0bad63051a33fdd95648900f75cead81ea83ebbf
Stela Capsa <stela.capsa@gmail.com>
2019-10-28 15:58:45 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,17 @@
+def display_board(board)
+ def valid_move?(board, index)
+  def position_taken?(board, index)
+   def input_to_index(user_input)
+    def move(board, index, first_player = "X")
+    end
+def turn(board)
+  puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
