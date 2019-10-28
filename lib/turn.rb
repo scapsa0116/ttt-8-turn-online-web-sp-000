8183c1e314db444ce014c52a1a093f5919fcd3d1
Stela Capsa <stela.capsa@gmail.com>
2019-10-28 16:03:57 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,8 +10,9 @@ def turn(board)
   index = input_to_index(num)
   if valid_move?(board, index) == true
     move(board, index)
-    display_board(board)
+    puts display_board(board)
   else
+    puts "invalid entry"
     turn(board)
   end
 end
