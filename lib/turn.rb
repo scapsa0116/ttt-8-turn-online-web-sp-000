08e590703a9d02f9a0c0211b56e1e694a9f2110d
Stela Capsa <stela.capsa@gmail.com>
2019-10-28 16:24:21 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,13 @@
+def display_board
+   puts row = ["   " "|" "   " "|" "   "]
+   puts separator = "-----------"
+   puts row
+   puts separator
+   puts row
+ end
 def display_board(board)
   puts board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
    puts " #{board[0]} | #{board[1]} | #{board[2]} "
    puts "-----------"
    puts " #{board[3]} | #{board[4]} | #{board[5]} "
