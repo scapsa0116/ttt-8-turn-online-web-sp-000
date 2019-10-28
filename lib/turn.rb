267ba895df583d8dd07bac47d3f02bfcbe574d96
Stela Capsa <stela.capsa@gmail.com>
2019-10-28 17:04:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,7 +9,7 @@ def display_board(board)
    puts "-----------"
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 puts "Welcome to Tic Tac Toe"
 puts display_board(board)
 turn(board)
