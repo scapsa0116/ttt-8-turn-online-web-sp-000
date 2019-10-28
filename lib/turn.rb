3cb125257dfdf903821c849736c659da52d628eb
Stela Capsa <stela.capsa@gmail.com>
2019-10-28 16:22:07 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,27 @@
-# def display_board(board)
-#  def valid_move?(board, index)
-#   def position_taken?(board, index)
-#    def input_to_index(user_input)
-#     def move(board, index, first_player = "X")
-#     end
+def display_board(board)
+  puts board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+def move(board, index, player)
+   board[index] = player
+end
+def position_taken?(board, index)
+   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+      return false
+   else
+      return true
+   end
+end
+def valid_move?(board, index)
+   if index.between?(0,8) && !position_taken?(board, index)
+      return true
+   end
+end
+display_board(board)
 def turn(board)
   puts "Please enter 1-9:"
   num = gets.chomp
